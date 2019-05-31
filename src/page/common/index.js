fed19e41f98ed43c3fc40f048e55756037fd6346
depers <dev_fengxiao@163.com>
2019-05-31 12:15:04 +0800

    4.9通用footer开发

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,10 +2,11 @@
  * @Author: depers(fengxiao) 
  * @Date: 2019-04-28 12:17:12 
  * @Last Modified by: depers
- * @Last Modified time: 2019-05-30 17:45:58
+ * @Last Modified time: 2019-05-31 12:11:20
  */
 
 'use strict';
 
 require('./layout.css')
+require('./footer/index.css');
 require('node_modules/font-awesome/css/font-awesome.min.css')
