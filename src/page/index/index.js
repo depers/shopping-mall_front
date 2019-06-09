bc3c62843b7fd0da151528c657ff07805883e7d3
depers <dev_fengxiao@163.com>
2019-06-09 12:20:34 +0800

    6.2首页左侧关键字导航界面开发

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,15 +2,14 @@
  * @Author: depers(fengxiao) 
  * @Date: 2019-04-28 11:38:07 
  * @Last Modified by: depers
- * @Last Modified time: 2019-06-04 17:00:24
+ * @Last Modified time: 2019-06-09 12:04:24
  */
 'use strict';
 
+require('./index.css');
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
 var navSide = require('page/common/nav-side/index.js');
 var _mm = require('util/mm.js');
 
-navSide.init({
-    name : 'order-list',
-});
+
