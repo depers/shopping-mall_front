7244c887d7cb4d908f6bbd7ff313b6b687b2fe67
depers <dev_fengxiao@163.com>
2019-06-10 18:18:51 +0800

    6.4 首页Banner,F楼层界面开发2

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,14 +2,30 @@
  * @Author: depers(fengxiao) 
  * @Date: 2019-04-28 11:38:07 
  * @Last Modified by: depers
- * @Last Modified time: 2019-06-09 12:04:24
+ * @Last Modified time: 2019-06-10 18:17:04
  */
 'use strict';
 
 require('./index.css');
 require('page/common/header/index.js');
 require('page/common/nav/index.js');
+require('util/slider/index.js');
 var navSide = require('page/common/nav-side/index.js');
+var templateBanner = require('./index.string');
 var _mm = require('util/mm.js');
 
-
+// 初始化banner
+$(function() {
+    // 渲染banner的html
+    var bannerHtml = _mm.renderHtml(templateBanner);
+    $('.banner-con').html(bannerHtml);
+    // 初始化banner
+    var $slider = $('.banner').unslider({
+        dots: true,
+    });
+    // 前一张和后一张事件绑定
+    $('.banner-con .banner-arrow').click(function(){
+        var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+        $slider.data('unslider')[forward]();
+    })
+});
---
+++ src/util/slider/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: depers 
+ * @Date: 2019-06-10 17:48:58 
+ * @Last Modified by: depers
+ * @Last Modified time: 2019-06-10 17:49:53
+ */
+'use strict'
+
+require('./index.css');
+require('./unslider.js');
